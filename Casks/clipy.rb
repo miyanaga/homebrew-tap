201c6c546230da74c4e720216f7cd532cce08439
Kunihiko Miyanaga <miyanaga@ideamans.com>
2026-07-13 09:23:46 +0900

    Add clipy cask

---
+++ Casks/clipy.rb
@@ -0,0 +1,25 @@
+cask "clipy" do
+  version "1.3.0"
+  sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+
+  url "https://github.com/miyanaga/Clipy/releases/download/v#{version}/Clipy-#{version}.zip"
+  name "Clipy"
+  desc "Clipboard extension app (miyanaga fork with encrypted snippet sync)"
+  homepage "https://github.com/miyanaga/Clipy"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  auto_updates false
+  depends_on macos: ">= :ventura"
+
+  app "Clipy.app"
+
+  caveats <<~EOS
+    This build is not notarized yet. Install with:
+      brew install --cask --no-quarantine miyanaga/tap/clipy
+    or set HOMEBREW_CASK_OPTS="--no-quarantine".
+  EOS
+end
---
+++ README.md
@@ -0,0 +1,13 @@
+# miyanaga/homebrew-tap
+
+Personal Homebrew tap.
+
+## Clipy (fork with encrypted snippet sync)
+
+```sh
+brew tap miyanaga/tap
+brew install --cask --no-quarantine miyanaga/tap/clipy
+```
+
+The cask is updated automatically by the release workflow in
+[miyanaga/Clipy](https://github.com/miyanaga/Clipy).
